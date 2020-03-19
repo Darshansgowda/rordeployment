8e2b48fcddda3fe4bd7ffa1e2e9d597d92e529ac
darshan405 <dshivakumar@qwinix.io>
2020-03-19 18:49:51 +0530

    second commit3

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.12.1"
 set :default_environment, { 
-  'PATH' => "/home/deploy/.rvm/gems/ruby-2.5.0/bin:/home/deploy/.rvm/gems/ruby-2.5.0@global/bin:/home/deploy/.rvm/rubies/ruby-2.5.0/bin:/home/deploy/.rvm/bin:/home/deploy/.nvm/versions/node/v6.11.5/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/deploy/.local/bin:/home/deploy/bin:$PATH",
+  'PATH' => "/u01/apps/qwinix/ruby-project",
   'RUBY_VERSION' => 'ruby 2.5.0',
   'GEM_HOME' => '/home/deploy/.rvm/gems/ruby-2.5.0',
   'GEM_PATH' => '/home/deploy/.rvm/gems/ruby-2.5.0' 
@@ -12,7 +12,7 @@ set :application, "rordeployment"
 set :repo_url, "git@github.com:Darshansgowda/rordeployment.git"
 set :branch, "master"
 
-set :deploy_to, '/home/deploy/rordeployment'
+set :deploy_to, '/u01/apps/qwinix/ruby-project'
 set :log_level, :debug
  
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets}
