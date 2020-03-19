1762d81629ea205a9143a65bfc4c4b14da0b34e3
darshan405 <dshivakumar@qwinix.io>
2020-03-19 19:14:54 +0530

    second commit4

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -15,4 +15,4 @@ forward_agent: true,
 auth_methods: %w(publickey)
 }
  
-server 'ec2-13-57-32-148.us-west-1.compute.amazonaws.com', user: 'ec2-user', roles: %w{web app db}
+server 'ec2-13-57-32-148.us-west-1.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
