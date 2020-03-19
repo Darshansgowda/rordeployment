e8674b23ace5445b39dd62f3c9b64e88b50d8eb2
darshan405 <dshivakumar@qwinix.io>
2020-03-19 19:43:42 +0530

    second commit11

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,8 @@
 # config/deploy/staging.rb
 
 server "13.57.32.148", user: "deploy", roles: %w{app db web}
- server "13.57.32.148", user: "deploy", roles: %w{app web} 
- server "13.57.32.148", user: "deploy", roles: %w{db}
+server "13.57.32.148", user: "deploy", roles: %w{app web} 
+# server "13.57.32.148", user: "deploy", roles: %w{db}
 
 set :branch, "master"
 set :stage, "staging"
