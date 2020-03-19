f9c4080a420c7e7ed120f662e9e532c19c077d75
darshan405 <dshivakumar@qwinix.io>
2020-03-19 19:21:37 +0530

    second commit5

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,18 +1,11 @@
 # config/deploy/staging.rb
-set :staging, :master
-set :unicorn_rack_env, "staging"
-set :rails_env, 'staging'
-set :migration_role, 'db'
-set :branch, "master"
 
-role :app, %w{ec2-13-57-32-148.us-west-1.compute.amazonaws.com} # Server IP Address
-role :web, %w{ec2-13-57-32-148.us-west-1.compute.amazonaws.com}
-role :db, %w{ec2-13-57-32-148.us-west-1.compute.amazonaws.com}
- 
-set :ssh_options, {
-keys: [File.expand_path('~/keys/jenkins-key.pem')],
-forward_agent: true,
-auth_methods: %w(publickey)
-}
- 
-server 'ec2-13-57-32-148.us-west-1.compute.amazonaws.com', user: 'deploy', roles: %w{web app db}
+server "13.57.32.148", user: "deploy", roles: %w{app db web}
+ server "13.57.32.148", user: "deploy", roles: %w{app web} 
+ server "13.57.32.148", user: "deploy", roles: %w{db}
+
+set :branch, "master"
+set :stage, "staging"
+role :app, %w{deploy@13.57.32.148}
+role :web, %w{deploy@13.57.32.148}
+role :db,  %w{deploy@13.57.32.148}
